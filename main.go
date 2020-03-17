b8ff2de41c251a40c2507e8234da980df58c745e
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2020-03-17 20:59:50 +0100

    add: repl
    Signed-off-by: Krzysztof Grodzicki <kgrodzicki@gmail.com>

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kgrodzicki/writing-an-interpreter-in-go/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language !\n", u.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
